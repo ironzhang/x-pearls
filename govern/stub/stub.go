96a682da347a38396e6a870f9ef64df143cc36cf
zhanghui <zhanghui@ablecloud.cn>
2018-05-25 17:26:20 +0800

    add govern/stub pkg

---
+++ govern/stub/stub.go
@@ -0,0 +1,129 @@
+package stub
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/ironzhang/x-pearls/govern"
+)
+
+const DriverName = "stub"
+
+type Provider struct {
+	dir  string
+	done chan struct{}
+}
+
+func NewProvider(dir string, interval time.Duration, f govern.GetEndpointFunc) *Provider {
+	if f == nil {
+		panic("govern.GetEndpointFunc is nil")
+	}
+
+	ch := make(chan struct{})
+	go func(done <-chan struct{}) {
+		t := time.NewTicker(interval)
+		defer t.Stop()
+
+		f()
+		for {
+			select {
+			case <-t.C:
+				f()
+			case <-done:
+				return
+			}
+		}
+	}(ch)
+
+	return &Provider{dir: dir, done: ch}
+}
+
+func (p *Provider) Driver() string {
+	return DriverName
+}
+
+func (p *Provider) Directory() string {
+	return p.dir
+}
+
+func (p *Provider) Close() error {
+	close(p.done)
+	return nil
+}
+
+type Consumer struct {
+	dir       string
+	endpoints []govern.Endpoint
+}
+
+func NewConsumer(dir string, endpoints []govern.Endpoint, refresh govern.RefreshEndpointsFunc) *Consumer {
+	if refresh != nil {
+		refresh(endpoints)
+	}
+	return &Consumer{
+		dir:       dir,
+		endpoints: endpoints,
+	}
+}
+
+func (p *Consumer) Driver() string {
+	return DriverName
+}
+
+func (p *Consumer) Directory() string {
+	return p.dir
+}
+
+func (p *Consumer) Close() error {
+	return nil
+}
+
+func (p *Consumer) GetEndpoints() []govern.Endpoint {
+	return p.endpoints
+}
+
+type Driver struct {
+	namespace string
+	endpoints []govern.Endpoint
+}
+
+func NewDriver(namespace string, endpoints []govern.Endpoint) *Driver {
+	return &Driver{namespace: namespace, endpoints: endpoints}
+}
+
+func (p *Driver) Name() string {
+	return DriverName
+}
+
+func (p *Driver) Namespace() string {
+	return p.namespace
+}
+
+func (p *Driver) NewProvider(service string, interval time.Duration, f govern.GetEndpointFunc) govern.Provider {
+	return NewProvider(p.dir(service), interval, f)
+}
+
+func (p *Driver) NewConsumer(service string, endpoint govern.Endpoint, f govern.RefreshEndpointsFunc) govern.Consumer {
+	return NewConsumer(p.dir(service), p.endpoints, f)
+}
+
+func (p *Driver) Close() error {
+	return nil
+}
+
+func (p *Driver) dir(service string) string {
+	return fmt.Sprintf("/%s/%s", p.namespace, service)
+}
+
+type Config struct {
+	Endpoints []govern.Endpoint
+}
+
+func Open(namespace string, config interface{}) (govern.Driver, error) {
+	c := config.(Config)
+	return NewDriver(namespace, c.Endpoints), nil
+}
+
+func init() {
+	govern.Register(DriverName, Open)
+}
